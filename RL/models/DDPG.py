3a5aa0393c8198f4d7953b67a8bbf6cfedcf3307
Armin <arminonline71@gmail.com>
2024-05-12 21:36:31 +0330

    Update

--- RL/models/DDPG.py
+++ RL/models/DDPG.py
@@ -4,6 +4,8 @@ import torch.nn as nn
 import torch.optim as optim
 import numpy as np
 import gymnasium as gym
+import torch.nn.functional as F
+
 
 class Actor(nn.Module):
     def __init__(self, state_dim, action_dim, max_action):
